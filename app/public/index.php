a18a1005bf711a15e184349e8c5e14d9ea53d5e5
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-03-12 17:46:20 +0100

    working

--- app/public/index.php
+++ app/public/index.php
@@ -9,7 +9,7 @@ ini_set("display_errors", 1);
 require __DIR__ . '/../vendor/autoload.php';
 
 // Create Router instance
-$router = new \Bdoramus\Router\Router();
+$router = new \Bramus\Router\Router();
 
 $router->setNamespace('Controllers');
 
